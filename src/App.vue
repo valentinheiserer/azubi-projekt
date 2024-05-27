c47cfaa920afbc6236046b5d9327f9c2ddd28a01
Valentin Heiserer <valentin.heiserer@multivac.de>
2024-05-27 15:21:40 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+import {newsType} from "./news.ts";
+import {news} from "./news.ts";
+import {ref} from "vue";
+import NewsComponent from "./components/NewsComponent.vue";
+
+const allNews = ref<newsType[]>(news);
+
+</script>
+
+<template>
+<news-component v-for="(news, index) in allNews" :key="index" :news="news"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/NewsComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import {newsType} from "../news.ts";
+
+defineProps<{
+  news: newsType;
+}>()
+</script>
+
+<template>
+  <div>
+    <h2>{{news.title}}</h2>
+    <p>{{news.description}}</p>
+    <img :src="news.img_url" alt="news_image">
+    <p>{{news.text}}</p>
+    <p>{{news.author}}</p>
+    <p>{{news.timestamp}}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
